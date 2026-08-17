1cfd35678fd3ce91d0be15fd3062f188bc150993
michaeljguarino <mjg@plural.sh>
2026-08-17 19:39:13 -0400

    add checksums

--- checksum-Elixir.ExMonty.Native.exs
+++ checksum-Elixir.ExMonty.Native.exs
@@ -1,6 +1,8 @@
 %{
-  "libex_monty-v0.5.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:618543708ac8b0515b564c7901a91363b1af331f1ae92c303fd55ac10de8e0b6",
-  "libex_monty-v0.5.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e763f70b293afe8d1daa32e06a0fd14682cbc25f0e9d57ffbb3fb2e65d90c6d8",
-  "libex_monty-v0.5.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:6a4edaa2b5e069019ec7fdacf1e83098296047b34f115c14ad2d5af702c6bf6a",
-  "libex_monty-v0.5.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d2f01fb4d865919e6c54ab048269db84efb237edfa457a75a064e076727d001f",
+  "libex_monty-v0.6.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bb9a892a6d383e198613076d11d33afb7d71b525da4bbb5afa8c0922846c800d",
+  "libex_monty-v0.6.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4113cbfdcaabeac0fe8b1e819df3739256a5549aa48b0ba82bfae6153266886c",
+  "libex_monty-v0.6.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:e6a4649bcae26e97f4a45b96371e9a1e4d890f42a4efd6e7d81b66ea71f35d0e",
+  "libex_monty-v0.6.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:bc465b062f8edafa87fd88eeea46a828174c4d6924c3a6c8bde6c2cdfe8744b9",
+  "libex_monty-v0.6.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:26afe112658ba86468f824ac4f5c76bfa5fed17159a9fb12c8ad424bbd3fe2dc",
+  "libex_monty-v0.6.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:fd5f85bf0b5e569d75ec606a796739aae237551de14e4f42b1e68fa0e8a2cae6",
 }
